af1ef4603c8cab4fc7052358d6c05e4fbd22c2e6
Alif Rachmawadi <code@subosito.com>
2016-01-26 15:39:58 +0700

    added more tests

--- lib/omniauth/strategies/bukalapak.rb
+++ lib/omniauth/strategies/bukalapak.rb
@@ -21,9 +21,32 @@ module OmniAuth
         end
       end
 
+      uid do
+        raw_info['id'].to_s
+      end
+
+      info do
+        hash = {
+          'username' => raw_info['username'],
+          'name'     => raw_info['name'],
+          'image'    => raw_info['avatar']['url']
+        }
+
+        hash['email'] = email unless email.nil?
+        hash
+      end
+
+      extra do
+        { raw_info: raw_info }
+      end
+
       def raw_info
         access_token.options[:mode] = :query
         @raw_info ||= access_token.get('me').parsed
+      end
+
+      def email
+        raw_info['email']
       end
     end
   end
--- spec/omniauth/strategies/bukalapak_spec.rb
+++ spec/omniauth/strategies/bukalapak_spec.rb
@@ -19,9 +19,73 @@ describe OmniAuth::Strategies::Bukalapak do
     it { expect(subject.options.client_options.token_url).to eq('https://www.bukalapak.com/oauth/access_token') }
   end
 
+  context "#id" do
+    before { expect(subject).to receive(:raw_info).and_return({ 'id' => 1234567 }) }
+
+    it { expect(subject.uid).to eq('1234567') }
+  end
+
+  context "#info" do
+    let(:basic_info) do
+      {
+        'username' => 'garuda',
+        'name'     => 'Garuda Pancasila',
+        'avatar'   => { 'url' => 'https://s1.bukalapak.com/image/garuda.png' }
+      }
+    end
+
+    let(:want_info) do
+      {
+        'username' => 'garuda',
+        'name'     => 'Garuda Pancasila',
+        'image'    => 'https://s1.bukalapak.com/image/garuda.png'
+      }
+    end
+
+    let(:want_info_with_email) do
+      want_info.merge('email' => 'user@example.com')
+    end
+
+    context "without email" do
+      let(:info) { basic_info }
+
+      before { expect(subject).to receive(:raw_info).and_return(info).exactly(4).times }
+
+      it { expect(subject.info).to eq(want_info) }
+    end
+
+    context "with email" do
+      let(:info) { basic_info.merge('email' => 'user@example.com') }
+
+      before { expect(subject).to receive(:raw_info).and_return(info).exactly(5).times }
+
+      it { expect(subject.info).to eq(want_info_with_email) }
+    end
+  end
+
+  context '#extra' do
+    before { expect(access_token).to receive(:get).with('me').and_return(response) }
+
+    it { expect(subject.extra).to eq(raw_info: parsed_response) }
+  end
+
   describe '#raw_info' do
     before { expect(access_token).to receive(:get).with('me').and_return(response) }
 
     it { expect(subject.raw_info).to eq(parsed_response) }
+  end
+
+  describe '#email' do
+    context "email present" do
+      before { expect(subject).to receive(:raw_info).and_return({ 'email' => 'user@example.com' }) }
+
+      it { expect(subject.email).to eq('user@example.com') }
+    end
+
+    context "email is not present" do
+      before { expect(subject).to receive(:raw_info).and_return({}) }
+
+      it { expect(subject.email).to be_nil }
+    end
   end
 end
